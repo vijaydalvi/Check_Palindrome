4688f5ca37293ab1182c81041f979df41a8965af
Vijay <vijaydalvi2525@gmail.com>
2022-04-18 11:24:43 +0530

    Add files via upload

---
+++ Check_Pailndrome_word.py
@@ -0,0 +1,8 @@
+str1=input("Enter Any Words:-")
+reverse=str1[::-1]
+if str1==reverse:
+    print(reverse)
+    print("This Is Palindrome")
+else:
+    print(reverse)
+    print("This Is Not Palindrome")
